6172714dc452166cc794a28ca8afbd9889dd5c4b
alvin <Origamiancientdragon@gmail.com>
2026-09-16 09:19:39 -0500

    Initial Commit

---
+++ collectionAppWei/collectionAppWeiApp.swift
@@ -0,0 +1,17 @@
+//
+//  collectionAppWeiApp.swift
+//  collectionAppWei
+//
+//  Created by ALVIN WEI on 9/16/26.
+//
+
+import SwiftUI
+
+@main
+struct collectionAppWeiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
